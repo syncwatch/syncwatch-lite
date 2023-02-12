2fd7317f6282f62e33ef762088a44055696f5a6a
skillor <skillor@gmx.net>
2023-02-12 16:59:37 +0100

    added noservice config

--- src/app/shared/service/service.worker.config.ts
+++ src/app/shared/service/service.worker.config.ts
@@ -1,21 +1,18 @@
-export let generatedServiceWorkerUrl: string;
+import { environment } from "src/environments/environment";
 
-(() => {
-    const _oldWorker = Worker;
-    // Override the real Worker with a stub
-    // to return the filename, which will be generated/replaced by the worker-plugin.
-    // @ts-ignore
-    Worker = class WorkerStub {
-        constructor(public stringUrl: string, public options?: WorkerOptions) {}
-    };
+const _oldWorker = Worker;
+// Override the real Worker with a stub
+// to return the filename, which will be generated/replaced by the worker-plugin.
+// @ts-ignore
+Worker = class WorkerStub {
+    constructor(public stringUrl: string, public options?: WorkerOptions) {}
+};
 
-    const worker = new Worker(new URL('./service.worker', import.meta.url)) as any;
-    generatedServiceWorkerUrl = worker.stringUrl;
-
-    Worker = _oldWorker;
-})();
+const worker = new Worker(new URL('./service.worker', import.meta.url)) as any;
+export const generatedServiceWorkerUrl = worker.stringUrl;
+Worker = _oldWorker;
 
 export const serviceWorkerConfig = {
-    enabled: true,
-    serviceWorkerUrl: generatedServiceWorkerUrl,
+  enabled: environment.enableService,
+  serviceWorkerUrl: generatedServiceWorkerUrl,
 };
